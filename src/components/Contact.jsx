adb2c85060a2e71fc10f457d6b68f778e7bb5e02
Kaleem <shaikaleem@deloitte.com>
2025-06-01 23:04:06 +0530

    form updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,7 @@ const Contact = () => {
                 ...data,
                 createdAt: new Date()
             })
+
             // console.log("Document written with ID: ", docRef.id);
             alert("Your response has securely sent!");
             reset();
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
         }
         catch (err) {
             console.log(err.code, err.message)
-        }
+        }   
     }
 
     return (
